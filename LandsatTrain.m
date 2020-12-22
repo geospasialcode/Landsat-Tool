7178a55b415cb27f614fb9350235a778411d464a
GeospatialCode <76454336+geospasialcode@users.noreply.github.com>
2020-12-22 10:53:40 +0800

    Main program

---
+++ LandsatTrain.m
@@ -0,0 +1,14 @@
+clc
+
+% Function
+W = readlandsat(1);
+X = readlandsat(2);
+Y = readlandsat(3);
+Z = readlandsat(4);
+
+% Class
+U = LandsatTool(1);
+U = LandsatTool(2,U);
+U = LandsatTool(3,U);
+
+
